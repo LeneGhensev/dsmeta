6a57bf7a2f273dcb008bae90540b4c36a60c8dbc
LeneGhensev <lenegghensev@gmail.com>
2022-07-14 21:37:11 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+  return(
+      <header>
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="logo DSMeta"/>
+            <h1>DSMeta</h1>
+            <p>Desenvolvido por 
+            <a href="https://github.com/LeneGhensev">Lene Ghensev</a></p>
+        </div>
+    </header>
+  )
+}
+
+export default Header       
